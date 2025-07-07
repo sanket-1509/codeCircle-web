5b56b1c672fef4e43d6b20145531f3c93944ffc4
Sanket Deshmukh <Sanket.Deshmukh@EMERGYS.LOCAL>
2025-07-07 22:33:41 +0530

    Refactor BASE_URL to dynamically set based on hostname

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "/api";
-export const USER_URL="https://img.freepik.com/premium-vector/anonymous-user-flat-icon-vector-illustration-with-long-shadow_520826-1932.jpg";
+export const BASE_URL =  location.hostname == "localhost" ? "http://localhost:3000" : "/api";
+
+export const USER_URL =
+  "https://img.freepik.com/premium-vector/anonymous-user-flat-icon-vector-illustration-with-long-shadow_520826-1932.jpg";
